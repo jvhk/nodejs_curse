882f7e545a67437232b0c25213f99b3153b773d5
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-01-12 04:28:38 -0300

    criando validações com express-validator3.0.2

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,13 +1,29 @@
+const { render } = require("../../config/server");
+
 module.exports = function(application){
 
     application.get('/formulario_inclusao_noticia', function(req,res){
+        //res.render("admin/form_add_noticia", {validacao: {}, noticia: {}});
         res.render("admin/form_add_noticia");
     });
 
     application.post('/noticias/salvar', function(req, res){
         let noticia = req.body;
         
+        req.assert('titulo','O título é um campo obrigatório!').notEmpty();
+        req.assert('resumo','O resumo é um campo obrigatório!').notEmpty();
+        req.assert('resumo','O resumo deve conter entre 10 e 100 caracteres!').len(10,100);
+        req.assert('autor','O autor é um campo obrigatório!').notEmpty();
+        req.assert('data_noticia','A data é um campo obrigatório!').notEmpty();
+        req.assert('noticia','A descrição é um campo obrigatório!').notEmpty();
         
+        let errors = req.validationErrors();
+
+        if(errors){
+            res.render("admin/form_add_noticia", {validacao : errors});
+            return; // para não executar os próximos passos
+        }
+
         //recuperando conec
         const connection = application.config.dbconnection();
         //recuperando o model
--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 const express  = require("express");
 const consign  = require("consign");
 const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.set("views", "./app/views");
 
 //middleware
 app.use(bodyParser.urlencoded({extended : true}));
+app.use(expressValidator());
 
 // settando o consign
 consign()

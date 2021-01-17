3b8bf81b8f689ee495152aea0021d198cef14573
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-01-16 21:44:37 -0300

    validacao dos campos da pagina index

--- projeto_multiroomchat/app/controllers/chat.js
+++ projeto_multiroomchat/app/controllers/chat.js
@@ -1,3 +1,17 @@
 module.exports.iniciaChat = function(application, req, res){
+
+    let dadosForm = req.body;
+
+    //validações
+    req.assert('apelido', 'Nome ou apelido é um campo obrigatório').notEmpty();
+    req.assert('apelido', 'Nome ou apelido é um campo que deve possuir de 3 a 15 caracteres').len(3, 15);
+
+    let errosMsgs = req.validationErrors();
+
+    if(errosMsgs){
+        res.render('index', {validacao : errosMsgs});
+        return;
+    }
+
     res.render("chat")
 }

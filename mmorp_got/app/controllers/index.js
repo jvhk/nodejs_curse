e89cd6d4b824d17d6619bc008b67336ac1f2cee4
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-01-22 17:28:04 -0300

    criação do controller de autenticação e validações

--- mmorp_got/app/controllers/index.js
+++ mmorp_got/app/controllers/index.js
@@ -3,3 +3,22 @@ module.exports.home = function(application, req, res){
 		res.render('index');
 
 }
+
+module.exports.autenticar = function(application, req, res){
+
+	let dadosForm = req.body;
+
+	//Validação
+	req.assert('usuario', 'Usuário é um campo obrigatório').notEmpty();
+	req.assert('senha', 'Senha é um campo obrigatório').notEmpty();
+
+	let erros = req.validationErrors();
+
+	if(erros){
+		res.render("index", {validacao : erros});
+		return;
+	}
+
+	res.send("Tudo ok para criar sessão");
+
+}

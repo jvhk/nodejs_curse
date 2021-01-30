cb10bb1e60dcd9f6664b880520883755ee5d7772
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-01-30 20:08:03 -0300

    inicio do projeto usando api com mongodb

---
+++ instagram_clone/api/server.js
@@ -0,0 +1,66 @@
+const express = require('express'),
+bodyParser = require('body-parser'),
+mongodb = require('mongodb').MongoClient,
+objectID = require('mongodb').ObjectId;
+
+let app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+let port = 3000;
+
+app.listen(port);
+
+let dbName = 'instagram';
+let mongoURL = 'mongodb://localhost:27017/' + dbName;
+
+let connMongoDB = function(data) {
+    mongodb.connect(mongoURL, { useNewUrlParser: true }, function(err, client) {
+        let db = client.db(dbName);
+        query(db, data);
+    client.close();
+});
+}
+
+function query(db, data) {
+let collection = db.collection(data.collection);
+    switch (data.operacao) {
+        case 'atualizar':
+            collection.update(data.where, data.set);
+        break;
+        case 'inserir':
+             collection.insertOne(data.dados, data.callback);
+        break;
+        case 'pesquisar':
+             collection.find(data.dados).toArray(data.callback);
+        break;
+        case 'remover':
+            data.where._id = objectID(data.where._id);
+            collection.remove(data.where, data.callback);
+        break;
+    }
+}
+
+console.log('Servidor HTTP escutando na porta ' + port);
+
+app.get('/', function(req, res){
+res.send({msg: 'Olá'});
+});
+
+app.post('/api', function(req, res){
+let data = req.body;
+let dados = {
+operacao: 'inserir',
+dados: data,
+collection: 'postagens',
+callback: function(err, records){
+  if (err) {
+    res.json(err);
+  } else {
+    res.json(records);
+  }
+}
+}
+connMongoDB(dados);
+});
